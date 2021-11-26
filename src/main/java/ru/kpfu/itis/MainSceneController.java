c562ca47a9ad55557b520b4a290cf11eed2f30cf
alyonaaDm <alyonaa.dm@mail.ru>
2021-11-26 23:18:52 +0300

    Initial commit

---
+++ src/main/java/ru/kpfu/itis/MainSceneController.java
@@ -0,0 +1,10 @@
+package ru.kpfu.itis;
+
+import javafx.fxml.FXML;
+
+public class MainSceneController {
+    @FXML
+    private void buttonClicked() {
+        System.out.println("Button clicked!");
+    }
+}
